855d264fea66cef1bba9751de11166b70a6aecc0
WanLum <295386487@qq.com>
2024-10-15 18:37:56 +0800

    fix: :bug: 更新配置及修复bug
    1.添加commitlint配置｜2.修改git-cz的执行命令

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,30 +1,7 @@
-export default {
-  // 继承的规则
-  extends: ['@commitlint/config-conventional'],
-  // @see: https://commitlint.js.org/#/reference-rules
+// commitlint.config.cjs
+module.exports = {
   rules: {
-    'subject-case': [0], // subject大小写不做校验
-
-    // 类型枚举，git提交type必须是以下类型
-    'type-enum': [
-      // 当前验证的错误级别
-      2,
-      // 在什么情况下进行验证，always表示一直进行验证
-      'always',
-      [
-        'feat', // 新增功能
-        'fix', // 修复缺陷
-        'docs', // 文档变更
-        'style', // 代码格式（不影响功能，例如空格、分号等格式修正）
-        'refactor', // 代码重构（不包括 bug 修复、功能新增）
-        'perf', // 性能优化
-        'test', // 添加疏漏测试或已有测试改动
-        'build', // 构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）
-        'ci', // 修改 CI 配置、脚本
-        'revert', // 回滚 commit
-        'chore' // 对构建过程或辅助工具和库的更改（不影响源文件、测试用例）
-      ]
-    ]
+    'header-max-length': () => [0,'always',72],
   },
   prompt: {
     messages: {
@@ -43,17 +20,17 @@ export default {
     },
     // prettier-ignore
     types: [
-      { value: "feat",name: "特性:     ✨  新增功能",emoji: ":sparkles:" },
-      { value: "fix",name: "修复:     🐛  修复缺陷",emoji: ":bug:" },
-      { value: "docs",name: "文档:     📝  文档变更",emoji: ":memo:" },
-      { value: "style",name: "格式:     💄  代码格式（不影响功能，例如空格、分号等格式修正）",emoji: ":lipstick:" },
-      { value: "refactor",name: "重构:     ♻️  代码重构（不包括 bug 修复、功能新增）",emoji: ":recycle:" },
-      { value: "perf",name: "性能:     ⚡️  性能优化",emoji: ":zap:" },
-      { value: "test",name: "测试:     ✅  添加疏漏测试或已有测试改动",emoji: ":white_check_mark:" },
-      { value: "build",name: "构建:     📦️  构建流程、外部依赖变更（如升级 npm 包、修改 vite 配置等）",emoji: ":package:" },
-      { value: "ci",name: "集成:     🎡  修改 CI 配置、脚本",emoji: ":ferris_wheel:" },
-      { value: "revert",name: "回退:     ⏪️  回滚 commit",emoji: ":rewind:" },
-      { value: "chore",name: "其他:     🔨  对构建过程或辅助工具和库的更改（不影响源文件、测试用例）",emoji: ":hammer:" },
+      { value: "feat",name: "特性: ✨  新增功能",emoji: ":sparkles:" },
+      { value: "fix",name: "修复: 🐛  修复缺陷",emoji: ":bug:" },
+      { value: "docs",name: "文档: 📝  文档变更",emoji: ":memo:" },
+      { value: "style",name: "格式: 💄  代码格式（不影响功能，例如空格、分号等格式修正）",emoji: ":lipstick:" },
+      { value: "refactor",name: "重构: ♻️   代码重构（不包括 bug 修复、功能新增）",emoji: ":recycle:" },
+      { value: "perf",name: "性能: ⚡️  性能优化",emoji: ":zap:" },
+      { value: "test",name: "测试: ✅  添加疏漏测试或已有测试改动",emoji: ":white_check_mark:" },
+      { value: "build",name: "构建: 📦️  构建流程、外部依赖变更（如升级 npm 包、修改 vite 配置等）",emoji: ":package:" },
+      { value: "ci",name: "集成: 🎡  修改 CI 配置、脚本",emoji: ":ferris_wheel:" },
+      { value: "revert",name: "回退: ⏪️  回滚 commit",emoji: ":rewind:" },
+      { value: "chore",name: "其他: 🔨  对构建过程或辅助工具和库的更改（不影响源文件、测试用例）",emoji: ":hammer:" },
     ],
     useEmoji: true,
     emojiAlign: 'center',
